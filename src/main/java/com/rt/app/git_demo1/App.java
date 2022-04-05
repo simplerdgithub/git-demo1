ed16f1c8284123dc6a1d4e78450d84d11cca7234
Rta Diaz <rtadiaz552@gmail.com>
2022-04-05 16:56:48 +0530

    added git-demo1 project to local repo

---
+++ src/main/java/com/rt/app/git_demo1/App.java
@@ -0,0 +1,17 @@
+package com.rt.app.git_demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void addData(String data){
+		System.out.println("data=" +data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
