f14d09c351bed778b3732396e73f47e558de4d70
Rta Diaz <rtadiaz552@gmail.com>
2022-04-05 17:16:36 +0530

    added constatnt class and chanfged in app.class

--- src/main/java/com/rt/app/git_demo1/App.java
+++ src/main/java/com/rt/app/git_demo1/App.java
@@ -9,9 +9,10 @@ public class App
 	
 	public void addData(String data){
 		System.out.println("data=" +data);
-		if(data.equals("java techie")){
-			System.out.println("welcome");
-		}
+		if(data.equals(Constant.DATA_TYPE)){
+			
+		System.out.println("welcome");
+	}
 	}
     public static void main( String[] args )
     {
---
+++ src/main/java/com/rt/app/git_demo1/Constant.java
@@ -0,0 +1,6 @@
+package com.rt.app.git_demo1;
+
+public class Constant {
+	public static  String DATA_TYPE ="Java-techie";
+
+}
